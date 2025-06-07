dcafaac05c2c71e10da968799fdc859e4f15ab29
ShreyasBhagat <shreyasbhagat98@gmail.com>
2025-06-08 02:14:27 +0530

    Updated the Flask API integration- 5

--- app/app.py
+++ app/app.py
@@ -2,16 +2,16 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-@app.route('/api/hello', methods =['GET'])
-def hello():
-    return jsonify({'Message': 'Hello World'}), 200
 
+@app.route('/info')
+def get_info():
+    return jsonify({"id": request.args.get("id"), "name": "Test User"})
 
-@app.route('/api/add', methods =['POST'])
+@app.route('/api/add', methods=['POST'])
 def add():
-    data = request.get_json()
-    result = data['a'] + data ['b']
-    return jsonify({'Result': result}), 200
+    data = request.json
+    result = data['a'] + data['b']
+    return jsonify({"result": result})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
